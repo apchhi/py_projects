f1ee35cc5aefba38dbdd840f97b85e1d95cee2e7
apchhi <139372331+apchhi@users.noreply.github.com>
2023-08-05 18:29:42 +0300

    Delete begin directory

--- begin/vychest_vremya.py
+++
@@ -1,2 +0,0 @@
-total_second = float(input('Enter value second: '))
-hours = total_second // 3600                                         minutes = (total_second // 60) % 60                                  second = total_second % 60                                           print('You enter ', hours, 'hours, ', minutes, 'minutes, ', second, 'second, ')
