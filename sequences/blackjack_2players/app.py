087c8561d1b9f05975d1dbc4b187a48a0deff5f3
apchhi <139372331+apchhi@users.noreply.github.com>
2024-06-27 12:55:39 +0000

    reload a folder "blackjack"

--- sequences/blackjack_2players/app.py
+++ sequences/blackjack_2players/app.py
@@ -6,52 +6,51 @@ from cards_value import deck, count_num, suit_list
 #suit_list = ['spades', 'worms', 'diamonds', 'crosses']
 def main():
     card_deck = create_deck()
-    stage = 0
+    stage = 1
     while len(card_deck) != 0:
-        answer_player1 = 'y'
-        answer_player2 = 'y'
-        counter_player1 = 0
-        counter_player2 = 0
-        stage += 1
         print('Stage', stage)
-        print('-------')
-
-        while (counter_player1 <= 21 or counter_player2 <= 21) or (answer_player1 == 'y' or answer_player2 == 'y'):
-            
-            
-            print()
-            if counter_player1 == 21:
-                 break
-            elif counter_player1 < 21:
-                print('Player 1')
-                answer_player1 = input('You need 1 card?(y/n): ')
-                counter_player1 = give_card(card_deck, counter_player1)
-            if counter_player2 == 21:
-                break
-            elif counter_player2 < 21:
-                print('Player 2')
-                answer_player2 = input('You need 1 more card?(y/n): ')
-                counter_player2 = give_card(card_deck, counter_player2)
-            print('Player 1:', counter_player1, 'points')
-            print('Player 2:', counter_player2, 'points')
-            
-            print()
-
-        if counter_player1 == 21 and counter_player2 == 21:
-            print('Draw.')
-        elif counter_player1 < 21 or counter_player2 < 21:
-            if counter_player1 > counter_player2 or counter_player2 > 21:
-                print('Player 1 win.')
-            elif counter_player1 < counter_player2 or counter_player1 > 21:
-                print('Player 2 win.')
-            elif counter_player1 == counter_player2: 
-                print('Draw.')
+        result_calculation(card_deck)
+        stage += 1
         
+        
+def result_calculation(card_deck):
+    answer_player1 = 'y'
+    answer_player2 = 'y'
+    counter_player1 = 0
+    counter_player2 = 0
+    
+    print('-------')
+    while (counter_player1 <= 21 or counter_player2 <= 21) or (answer_player1 == 'y' or answer_player2 == 'y'):
+        print()
+        if counter_player1 == 21:
+            break
+        elif counter_player1 < 21:
+            print('Player 1')
+            answer_player1 = input('You need 1 card?(y/n): ')
+            counter_player1 = give_card(card_deck, counter_player1)
+        if counter_player2 == 21:
+            break
+        elif counter_player2 < 21:
+            print('Player 2')
+            answer_player2 = input('You need 1 more card?(y/n): ')
+            counter_player2 = give_card(card_deck, counter_player2)
+        print('Player 1:', counter_player1, 'points')
+        print('Player 2:', counter_player2, 'points')
+        print()
 
+    if counter_player1 == 21 and counter_player2 == 21:
+        print('Draw.')
+    elif counter_player1 < 21 or counter_player2 < 21:
+        if counter_player1 > counter_player2 or counter_player2 > 21:
+            print('Player 1 win.')
+        elif counter_player1 < counter_player2 or counter_player1 > 21:
+            print('Player 2 win.')
+        elif counter_player1 == counter_player2: 
+            print('Draw.')
               
 def give_card(card_deck, counter):
             card_player = random.choice(list(card_deck))
-            value_card = card_deck[card_player]
+            value_card = card_deck.pop(card_player)
             if card_player[0] == 'tuz':
                 if counter + value_card[1] <= 21:
                     value_card = value_card[1]
